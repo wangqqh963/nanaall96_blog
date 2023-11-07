174aad8a459ac3d45c28e22115d94a41c35e1e10
nanaall <105529590+wangqqh963@users.noreply.github.com>
2023-11-07 17:47:28 +0800

    Add files via upload

---
+++ html/js/leftnav.js
@@ -0,0 +1,38 @@
+document.write(`
+<div class="left">
+<div class="lnav">
+    <ul>
+        <div class='leftsss'>
+            <span class="head"></span>
+        </div>
+        <li class="nana">nanaall96_blog</li>
+        <span class="firstli"></span>
+        <li>
+            <a href="blog.html">
+            <span class="nav-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></span>
+            <span>首页</span>
+            </a>
+        </li>
+        <li class="diary">
+            <a href="diary.html">
+            <span class="nav-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg></span>
+            <span>日志</span>
+            </a>
+        </li>
+        <li class="useful">
+            <a href="useful.html">
+            <span class="nav-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg></span>
+            <span>useful</span>
+            </a>
+        </li>
+    </ul>
+</div>
+<div class="lnav n_lnav">
+    <ul>
+        <li class="motto">
+            吃饱喝饱，<br>一路走好~
+        </li>
+    </ul>
+</div>
+</div>
+`);
---
+++ html/js/my.js
@@ -0,0 +1,5 @@
+let s = prompt('输入音乐直链:')
+s !=='' ? document.write(`<audio class='oderm' src=${s} autoplay controls></audio>`) : alert(`叫你输链接!笨蛋!`)
+
+
+
